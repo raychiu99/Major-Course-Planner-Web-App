8b889075d3efb3e974e40db307d517d436ea9a19
Spencer Loi <sloi@ucsc.edu>
2022-02-04 09:42:47 -0800

    did some background stuff

--- major_planner/src/App.js
+++ major_planner/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import TemporaryDrawer from './temp.js';
+import background from './white-abstract-background.png'
 
 function App() {
   return (
-    <div>
-      <TemporaryDrawer/>
+    <div className="Major-Course-Planner">
+    <h1>Major Course Planner</h1>
+      <div>
+        <TemporaryDrawer/>
+      </div>
     </div>
   );
 }
