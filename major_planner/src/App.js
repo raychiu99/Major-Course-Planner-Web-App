d5872ca157d3f1030347af97b222194d854908c2
Spencer Loi <sloi@ucsc.edu>
2022-02-09 00:54:26 -0800

    added pages, sign up and sign in are functional

--- major_planner/src/App.js
+++ major_planner/src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import SignIn from './components/SignIn';
+import SignUp from "./components/SignUp";
+
 
 function App() {
+  
   return (
-    <SignIn />
+    <Router>
+      <div className="App">
+
+        <div className="Content">
+          <Switch>
+            <Route exact path="/">
+              <SignIn />
+            </Route>
+
+            <Route exact path="/SignUp">
+              <SignUp />
+            </Route>
+          </Switch>
+          
+        </div>
+      </div>
+    </Router>
   );
+  
 }
 
 export default App;
