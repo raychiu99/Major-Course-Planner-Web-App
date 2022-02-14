41611cbc2bad40abf47a321bc2a3b1ce485f705f
Ian Smith <smith.it.personal@gmail.com>
2022-02-14 15:42:43 -0800

    Added Dashboard.js

--- major_planner/src/App.js
+++ major_planner/src/App.js
@@ -3,23 +3,25 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SignIn from './components/SignIn';
 import SignUp from "./components/SignUp";
 import Navbar from "./components/Navbar";
-import {AuthProvider} from './contexts/AuthContext';
+import Dashboard from './components/Dashboard';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
     <AuthProvider>
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="Content">
-          <Switch>
-            <Route exact path="/" component={SignIn} />
-            <Route exact path="/SignUp" component={SignUp} />
-          </Switch> 
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="Content">
+            <Switch>
+              <Route exact path="/" component={SignIn} />
+              <Route exact path="/SignUp" component={SignUp} />
+              <Route exact path="/Home" component={Dashboard} />
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
-    </AuthProvider> 
+      </Router>
+    </AuthProvider>
   );
 }
 
---
+++ major_planner/src/components/Dashboard.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useAuth } from '../contexts/AuthContext'
+import { useState } from 'react';
+import { Container } from '@mui/material';
+
+const theme = createTheme();
+
+export default function Dashboard() {
+    let dashInfo = { // TODO: Replace with actual info
+        major: 'Computer Science',
+        status: 'Senior',
+        catalog: '2018-2019'
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container>
+                <Grid container spacing={2} >
+                    <Grid item xs={4} direction="column">
+                        <Paper style={{ height: "100%" }}>TEST</Paper>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={6}>
+                                <Paper>TEST</Paper>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Paper>test</Paper>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Paper>TEST</Paper>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Paper>test</Paper>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Container>
+        </ThemeProvider>
+    );
+}
