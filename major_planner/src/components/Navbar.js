bf442471ff293b7f101e317c68d6b77aba4d3734
Spencer Loi <sloi@ucsc.edu>
2022-03-05 21:30:19 -0800

    cleaned up appearance

--- major_planner/src/components/Navbar.js
+++ major_planner/src/components/Navbar.js
@@ -1,9 +1,9 @@
 const Navbar = () => {
 	return (
-		<div className='navbar-container' style={{ backgroundColor: '#f2ecde', textAlign: 'center' }}>
+		<div className='navbar-container' style={{ backgroundColor: '#f2ecde', textAlign: 'center', height:'100px'}}>
 			<nav className="navbar" style={{ display: 'inline-block', textAlign: 'center' }}>
 				<h1>Major Course Planner</h1>
-				<div className="links" style={{ textAlign: 'center' }}>
+				<div className="links" style={{ textAlign: 'center', paddingTop: '8px' }}>
 					<a href="/How-it-works">How It Works</a>
 					<a href="/SignUp">Sign Up</a>
 					<a href="/">Log In</a>
